2a51c4674afd71acd353ca975e2193ea3f1019de
Alex Carter <60497471+acarter881@users.noreply.github.com>
2023-08-31 16:42:12 -0500

    Update FRED_series_updates.py

--- FRED_series_updates.py
+++ FRED_series_updates.py
@@ -5,7 +5,7 @@ import pandas as pd
 from pandas.errors import EmptyDataError
 
 # Set filename
-FILENAME = 'seriess_data_3.csv'
+FILENAME = 'seriess_data_4.csv'
 
 # Retrieve FRED API key from the appropriate User variable
 API_KEY = os.environ['FRED_API_KEY']
